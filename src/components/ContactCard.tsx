28c557f5b18276c584e657dcc569f0667e3a838c
LeeSF03 <shuenfeilee@gmail.com>
2025-02-05 00:32:06 +0800

    feat: add alt prop to contact card

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -8,19 +8,21 @@ type SkillCardProps = {
   img: string
   link: string
   className?: string
+  alt?: string
 }
 export const ContactCard = ({
   title,
   img,
   link,
   className,
+  alt,
 }: SkillCardProps) => {
   return (
     <div className={cn('flex-grow p-2', className)}>
       <Card className="flex h-60 flex-col items-center justify-center border-4 border-gray-800 bg-orange-200 p-5 shadow-[5px_5px_lightblue] hover:-motion-translate-y-out-[10px]">
         <Link href={link}>
           <CardContent className="flex h-48 items-center">
-            <Image src={img} alt={title} width={80} height={80} />
+            <Image src={img} alt={alt || ''} width={80} height={80} />
           </CardContent>
           <CardFooter className="justify-center">
             <CardTitle>{title}</CardTitle>
