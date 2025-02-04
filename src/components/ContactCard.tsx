cab2bffd4b779c3ec07c49ecc6fddc697bab52d1
LeeSF03 <shuenfeilee@gmail.com>
2025-02-04 23:14:24 +0800

    feat: change contact card design

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -1,14 +1,14 @@
-import { Card, CardContent, CardFooter, CardTitle } from "@/components/ui/card";
-import { cn } from "@/lib/utils";
-import Image from "next/image";
-import Link from "next/link";
+import { Card, CardContent, CardFooter, CardTitle } from '@/components/ui/card'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
 
 type SkillCardProps = {
-  title: string;
-  img: string;
-  link: string;
-  className?: string;
-};
+  title: string
+  img: string
+  link: string
+  className?: string
+}
 export const ContactCard = ({
   title,
   img,
@@ -16,10 +16,10 @@ export const ContactCard = ({
   className,
 }: SkillCardProps) => {
   return (
-    <div className={cn("flex-grow p-2", className)}>
-      <Card className="flex flex-col items-center justify-center p-5 h-60 hover:-motion-translate-y-out-[10px]">
+    <div className={cn('flex-grow p-2', className)}>
+      <Card className="flex h-60 flex-col items-center justify-center border-4 border-gray-800 bg-orange-200 p-5 shadow-[5px_5px_lightblue] hover:-motion-translate-y-out-[10px]">
         <Link href={link}>
-          <CardContent className="h-48 flex items-center">
+          <CardContent className="flex h-48 items-center">
             <Image src={img} alt={title} width={80} height={80} />
           </CardContent>
           <CardFooter className="justify-center">
@@ -28,5 +28,5 @@ export const ContactCard = ({
         </Link>
       </Card>
     </div>
-  );
-};
+  )
+}
