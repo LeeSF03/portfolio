601f6682d36366d243e6377bab2fe4487f21f451
LeeSF03 <shuenfeilee@gmail.com>
2025-02-04 23:13:52 +0800

    feat: change bg image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,34 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
-  title: "LeeSF - Programmer",
-  description: "LeeSF Software Engineering Experiences and Projects",
-};
+  title: 'LeeSF - Programmer',
+  description: 'LeeSF Software Engineering Experiences and Projects',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-[url(/img/background.jpg)] bg-fixed antialiased`}
       >
         {children}
       </body>
     </html>
-  );
+  )
 }
