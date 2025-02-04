9744fe372ca7584aa7ba27e365710c68fbccb89e
LeeSF03 <shuenfeilee@gmail.com>
2025-02-04 23:14:42 +0800

    feat: change top bar design

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,35 +1,39 @@
-import { Menubar, MenubarMenu, MenubarTrigger } from "@/components/ui/menubar";
-import { RefObject } from "react";
+import { Menubar, MenubarMenu, MenubarTrigger } from '@/components/ui/menubar'
+import { RefObject } from 'react'
 
 export type MenuItemWithRef = {
-  title: string;
-  ref: RefObject<HTMLDivElement | null>;
-};
+  title: string
+  ref: RefObject<HTMLDivElement | null>
+}
 
 type TopbarProps = {
-  menuItemsWithRef: MenuItemWithRef[];
-};
+  menuItemsWithRef: MenuItemWithRef[]
+}
 
 export const Topbar = ({ menuItemsWithRef }: TopbarProps) => {
   return (
-    <div className="flex justify-center w-full border border-slate-200 py-2 bg-white dark:border-slate-800 dark:bg-slate-950 sticky top-0">
-      <Menubar className="border-none max-w-5xl">
-        <MenubarMenu>
-          {menuItemsWithRef.map(({ title, ref }, index) => (
-            <MenubarTrigger
-              key={index}
-              className={"text-lg hover:bg-black hover:text-white"}
-              onClick={() =>
-                ref.current?.scrollIntoView({
-                  behavior: "smooth",
-                })
-              }
-            >
-              {title}
-            </MenubarTrigger>
-          ))}
-        </MenubarMenu>
-      </Menubar>
+    <div className="top-0 flex justify-center bg-none px-5 py-2">
+      <div className="w-full max-w-5xl rounded-xl border-4 border-gray-800 bg-blue-400 p-2 shadow-[7px_7px]">
+        <Menubar className="w-full justify-center rounded-lg border-4 border-gray-800 bg-orange-200 py-5">
+          <MenubarMenu>
+            {menuItemsWithRef.map(({ title, ref }, index) => (
+              <MenubarTrigger
+                key={index}
+                className={
+                  'h-8 bg-none text-lg font-bold hover:bg-transparent hover:-motion-translate-y-out-[3px]'
+                }
+                onClick={() =>
+                  ref.current?.scrollIntoView({
+                    behavior: 'smooth',
+                  })
+                }
+              >
+                {title}
+              </MenubarTrigger>
+            ))}
+          </MenubarMenu>
+        </Menubar>
+      </div>
     </div>
-  );
-};
+  )
+}
