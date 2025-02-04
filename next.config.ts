4f9669955f3567f5a6b259c30497e040d2bc80d2
LeeSF03 <shuenfeilee@gmail.com>
2025-02-04 23:18:27 +0800

    refactor: docker prod yaml and next config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+
+// const prefix = process.env.NODE_ENV === 'production' ? 'https://leesf.xyz' : ''
 
 const nextConfig: NextConfig = {
-  // assetPrefix: "http://localhost:8080",
-  assetPrefix: "https://leesf.xyz",
-};
+  assetPrefix: 'https://leesf.xyz',
+  // assetPrefix: 'http://localhost:8080',
+}
 
-export default nextConfig;
+export default nextConfig
