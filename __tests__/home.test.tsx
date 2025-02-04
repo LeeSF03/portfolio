0445cda3c779b69c92f8c6861fc9cffbea7b616e
LeeSF03 <shuenfeilee@gmail.com>
2025-02-05 00:33:43 +0800

    test: add test for skill card and contact card

--- __tests__/home.test.tsx
+++ __tests__/home.test.tsx
@@ -1,16 +1,44 @@
-import { expect, test } from "vitest";
-import { render, screen } from "@testing-library/react";
-import { Topbar, MenuItemWithRef } from "@/components/Topbar";
-import { createRef } from "react";
+import { expect, test } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import { Topbar, MenuItemWithRef } from '@/components/Topbar'
+import { createRef } from 'react'
+import { SkillCard } from '@/components/SkillCard'
+import { ContactCard } from '@/components/ContactCard'
 
-test("Test Home page", async () => {
-  const mockRef = createRef<HTMLDivElement | null>();
+test('Test Topbar component', async () => {
+  const mockRef = createRef<HTMLDivElement | null>()
   const menuItems: MenuItemWithRef[] = [
-    { title: "About Me", ref: mockRef },
-    { title: "Skills", ref: mockRef },
-    { title: "Contacts", ref: mockRef },
-  ];
+    { title: 'About Me', ref: mockRef },
+    { title: 'Skills', ref: mockRef },
+    { title: 'Contacts', ref: mockRef },
+  ]
 
-  const { getByText } = render(<Topbar menuItemsWithRef={menuItems} />);
-  expect(getByText("About Me")).toBeDefined();
-});
+  const { getByText } = render(<Topbar menuItemsWithRef={menuItems} />)
+  expect(getByText('About Me')).toBeDefined()
+  expect(getByText('Skills')).toBeDefined()
+  expect(getByText('Contacts')).toBeDefined()
+})
+
+test('Test SkillCard component', () => {
+  const { getByText, getByAltText } = render(
+    <SkillCard title={'Card Title'} img={''} alt={'alt text'} />
+  )
+
+  expect(getByText('Card Title')).toBeDefined()
+  expect(getByAltText('alt text')).toBeDefined()
+})
+
+test('Test ContactCard component', () => {
+  const { getByText, getByAltText, getByRole, getByDisplayValue } = render(
+    <ContactCard
+      link={'https://test/'}
+      title={'Card Title'}
+      img={''}
+      alt={'alt text'}
+    />
+  )
+
+  expect(getByRole('link')).toHaveProperty('href', 'https://test/')
+  // expect(getByDisplayValue('Card Title')).toBeDefined()
+  // expect(getByAltText('alt text')).toBeDefined()
+})
