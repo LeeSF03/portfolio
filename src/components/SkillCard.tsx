ac656b293128793f74e1692f2d78aeb11dbe67df
LeeSF03 <shuenfeilee@gmail.com>
2025-02-05 17:07:16 +0800

    feat: change card shadow position

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -11,7 +11,7 @@ type SkillCardProps = {
 export const SkillCard = ({ title, img, className, alt }: SkillCardProps) => {
   return (
     <div className={cn('flex-grow p-2', className)}>
-      <Card className="flex h-60 flex-col items-center justify-center border-4 border-gray-800 bg-red-200 p-5 shadow-[5px_5px_lightblue] hover:-motion-translate-y-out-[10px]">
+      <Card className="flex h-60 flex-col items-center justify-center border-4 border-gray-800 bg-red-200 p-5 shadow-[9px_9px_lightblue] hover:-motion-translate-y-out-[10px]">
         <CardContent className="flex h-48 items-center">
           <Image src={img} alt={alt || ''} width={80} height={80} />
         </CardContent>
