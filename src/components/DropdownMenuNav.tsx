5655cc9b2f3d8a881dc879f6e9354c3be3db9679
LeeSF03 <shuenfeilee@gmail.com>
2025-02-07 00:37:45 +0800

    feat: modify dropdown menu component look

--- src/components/DropdownMenuNav.tsx
+++ src/components/DropdownMenuNav.tsx
@@ -6,19 +6,36 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
+import Link from 'next/link'
 
-export const DropdownMenuNav = () => {
+type Page = {
+  name: string
+  path: string
+}
+type DropdownMenuNavProps = {
+  pages: Page[]
+}
+
+export const DropdownMenuNav = ({ pages }: DropdownMenuNavProps) => {
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger>Open</DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
-      </DropdownMenuContent>
+      <div className="group">
+        <div className="rounded-xl border-4 border-gray-800 bg-blue-400 p-1 shadow-[5px_5px] hover:cursor-pointer group-hover:-motion-translate-x-out-[5px] group-hover:-motion-translate-y-out-[5px] group-active:-motion-translate-x-out-[5px] group-active:-motion-translate-y-out-[5px]">
+          <DropdownMenuTrigger className="rounded-md border-4 border-gray-800 bg-purple-300 px-2 py-0.5 font-bold">
+            Open
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="m-2 rounded-md border-4 border-gray-800 bg-green-300 p-2 shadow-[5px_5px]">
+            {pages.map(({ name, path }) => (
+              <DropdownMenuItem
+                key={name}
+                className="font-bold hover:cursor-pointer hover:-motion-translate-x-out-[3px] hover:-motion-translate-y-out-[3px]"
+              >
+                <Link href={path}>{name}</Link>
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </div>
+      </div>
     </DropdownMenu>
   )
 }
