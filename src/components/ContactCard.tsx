c034b6b05522a5d531c7c1573244ae6d2d3e5d27
LeeSF03 <shuenfeilee@gmail.com>
2025-02-07 00:39:30 +0800

    feat: add onclick prop to contact card

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -10,6 +10,7 @@ type SkillCardProps = {
   link: string
   className?: ClassValue
   alt?: string
+  onClick?: () => void
 }
 export const ContactCard = ({
   title,
@@ -17,11 +18,12 @@ export const ContactCard = ({
   link,
   className,
   alt,
+  onClick,
 }: SkillCardProps) => {
   return (
     <div className={cn('flex-grow p-2', className)}>
       <Card className="flex h-60 flex-col items-center justify-center border-4 border-gray-800 bg-orange-200 p-5 shadow-[9px_9px_lightblue] hover:-motion-translate-y-out-[10px]">
-        <Link href={link}>
+        <Link href={link} onClick={onClick}>
           <CardContent className="flex h-48 items-center">
             <Image src={img} alt={alt || ''} width={80} height={80} />
           </CardContent>
