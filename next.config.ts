1c5c49f16f5db1d06cf5bb46c34234f92e4afd29
Lee Shuen Fei <109432084+LeeSF03@users.noreply.github.com>
2025-02-08 00:23:25 +0800

    fix: update nextjs config (#19)
    * ci: add auto deployment
    
    * style: small caps for job name
    
    * style: small caps for job name
    
    * ci: deploy after image is built
    
    * ci: fix deploy container ssh step
    
    * ci: fix deploy container ssh step
    
    * ci: fix deploy container ssh step
    
    * fix: remove env file from gitignore
    
    * ci: copy yarn lock file in dockerfile
    
    * feat: change classname type
    
    * refactor: appropriate padding usage
    
    * feat: add dropdown menu from shadcn ui
    
    * feat: add dropdown menu component initial style
    
    * fix: undefined root env from process
    
    * refactor: topbar shadow smaller
    
    * feat: modify dropdown menu component look
    
    * refactor: remove unused class in dropdown menu ui components
    
    * feat: a tidier about me section
    
    * feat: add dropdown menu in root layout
    
    * feat: add onclick prop to contact card
    
    * feat: add star image for about me section
    
    * chore: move dropdown menu from layout to page
    
    * chore: add page constant for dropdown navigation
    
    * refactor: add publicRoot constant
    
    * feat: finish styling dropdown nav
    
    * chore: add path to pages nav
    
    * chore: update props passed to dropdown nav
    
    * chore: add first blog cover image
    
    * refactor: move dropdown menu nav to root layout
    
    * feat: add custom font in tailwind
    
    * feat: add blog page with dummy healine
    
    * chore: add assetPrefix conditional base on NODE_ENV
    
    * chore: remove PUBLIC_URL env variable
    
    * chore: update dummy headline cover picture
    
    * fix: remove unused variable
    
    * ci: remove unused variable
    
    * feat: new facivon
    
    * fix: background missing
    
    * fix: background missing
    
    * fix: background missing
    
    * fix: missing cover
    
    * fix: missing cover
    
    * fix: missing cover
    
    * fix: udpate nginx.conf
    
    * fix: missing cover
    
    * fix: update nginx conf
    
    * fix: update nextjs config

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,17 @@ const prefix = process.env.NODE_ENV === 'production' ? 'https://leesf.xyz' : ''
 const nextConfig: NextConfig = {
   // assetPrefix: 'https://leesf.xyz',
   assetPrefix: prefix,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'leesf.xyz',
+        port: '443',
+        pathname: 'img/**',
+        search: '',
+      },
+    ],
+  },
 }
 
 export default nextConfig
