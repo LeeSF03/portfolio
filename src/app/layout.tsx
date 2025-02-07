e22a9ceb09e89c34b8f295fcf4ce2e4bb07a8fa9
LeeSF03 <shuenfeilee@gmail.com>
2025-02-07 22:41:03 +0800

    fix: background missing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,11 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { DropdownMenuNav } from '@/components/DropdownMenuNav'
 
+const bg =
+  process.env.NODE_ENV === 'production'
+    ? 'bg-[url(https://leesf.xyz/img/background.jpg)]'
+    : 'bg-[url(/img/background.jpg)]'
+
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
@@ -26,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} bg-[url(https://leesf.xyz/img/background.jpeg)] bg-fixed antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${bg} bg-fixed antialiased`}
       >
         <div className="flex w-full justify-center">
           <div className="flex w-full max-w-5xl flex-col p-7">
