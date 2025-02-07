08ed3ccac79a314625daafa337bb23da2a79e065
LeeSF03 <shuenfeilee@gmail.com>
2025-02-07 21:56:05 +0800

    chore: add first blog cover image

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next'
 // const prefix = process.env.NODE_ENV === 'production' ? 'https://leesf.xyz' : ''
 
 const nextConfig: NextConfig = {
-  assetPrefix: 'https://leesf.xyz',
+  // assetPrefix: 'https://leesf.xyz',
   // assetPrefix: 'http://localhost:8080',
 }
 
