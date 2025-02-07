0e9e6c4987153f8b15d2775da6a7289e5730ef57
LeeSF03 <shuenfeilee@gmail.com>
2025-02-07 14:09:54 +0800

    feat: finish styling dropdown nav

--- src/components/DropdownMenuNav.tsx
+++ src/components/DropdownMenuNav.tsx
@@ -2,10 +2,9 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
+import { cn } from '@/lib/utils'
 import Link from 'next/link'
 
 type Page = {
@@ -14,21 +13,31 @@ type Page = {
 }
 type DropdownMenuNavProps = {
   pages: Page[]
+  currentPageIndex: number
 }
 
-export const DropdownMenuNav = ({ pages }: DropdownMenuNavProps) => {
+export const DropdownMenuNav = ({
+  pages,
+  currentPageIndex,
+}: DropdownMenuNavProps) => {
   return (
     <DropdownMenu>
       <div className="group">
         <div className="rounded-xl border-4 border-gray-800 bg-blue-400 p-1 shadow-[5px_5px] hover:cursor-pointer group-hover:-motion-translate-x-out-[5px] group-hover:-motion-translate-y-out-[5px] group-active:-motion-translate-x-out-[5px] group-active:-motion-translate-y-out-[5px]">
           <DropdownMenuTrigger className="rounded-md border-4 border-gray-800 bg-purple-300 px-2 py-0.5 font-bold">
-            Open
+            {pages[currentPageIndex].name}
           </DropdownMenuTrigger>
-          <DropdownMenuContent className="m-2 rounded-md border-4 border-gray-800 bg-green-300 p-2 shadow-[5px_5px]">
-            {pages.map(({ name, path }) => (
+          <DropdownMenuContent className="m-2 space-y-1 rounded-md border-4 border-gray-800 bg-green-300 p-2 shadow-[5px_5px]">
+            {pages.map(({ name, path }, index) => (
               <DropdownMenuItem
                 key={name}
-                className="font-bold hover:cursor-pointer hover:-motion-translate-x-out-[3px] hover:-motion-translate-y-out-[3px]"
+                className={cn(
+                  'font-bold hover:cursor-pointer hover:-motion-translate-x-out-[3px] hover:-motion-translate-y-out-[3px]',
+                  {
+                    'border-2 border-gray-800 bg-red-300':
+                      currentPageIndex === index,
+                  }
+                )}
               >
                 <Link href={path}>{name}</Link>
               </DropdownMenuItem>
