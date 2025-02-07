fdcfb86d6621147e55a8264ad7586e18948f4f6a
Lee Shuen Fei <109432084+LeeSF03@users.noreply.github.com>
2025-02-07 22:38:44 +0800

    fix: background missing (#14)
    * ci: add auto deployment
    
    * style: small caps for job name
    
    * style: small caps for job name
    
    * ci: deploy after image is built
    
    * ci: fix deploy container ssh step
    
    * ci: fix deploy container ssh step
    
    * ci: fix deploy container ssh step
    
    * fix: remove env file from gitignore
    
    * ci: copy yarn lock file in dockerfile
    
    * feat: change classname type
    
    * refactor: appropriate padding usage
    
    * feat: add dropdown menu from shadcn ui
    
    * feat: add dropdown menu component initial style
    
    * fix: undefined root env from process
    
    * refactor: topbar shadow smaller
    
    * feat: modify dropdown menu component look
    
    * refactor: remove unused class in dropdown menu ui components
    
    * feat: a tidier about me section
    
    * feat: add dropdown menu in root layout
    
    * feat: add onclick prop to contact card
    
    * feat: add star image for about me section
    
    * chore: move dropdown menu from layout to page
    
    * chore: add page constant for dropdown navigation
    
    * refactor: add publicRoot constant
    
    * feat: finish styling dropdown nav
    
    * chore: add path to pages nav
    
    * chore: update props passed to dropdown nav
    
    * chore: add first blog cover image
    
    * refactor: move dropdown menu nav to root layout
    
    * feat: add custom font in tailwind
    
    * feat: add blog page with dummy healine
    
    * chore: add assetPrefix conditional base on NODE_ENV
    
    * chore: remove PUBLIC_URL env variable
    
    * chore: update dummy headline cover picture
    
    * fix: remove unused variable
    
    * ci: remove unused variable
    
    * feat: new facivon
    
    * fix: background missing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,6 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { DropdownMenuNav } from '@/components/DropdownMenuNav'
 
-const bg =
-  process.env.NODE_ENV === 'production'
-    ? 'bg-[url(https://leesf.xyz/img/background.jpeg)]'
-    : 'bg-[url(/img/background.jpg)]'
-
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
@@ -31,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${bg} bg-fixed antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-[url(https://leesf.xyz/img/background.jpeg)] bg-fixed antialiased`}
       >
         <div className="flex w-full justify-center">
           <div className="flex w-full max-w-5xl flex-col p-7">
