cc07f0b498210933ae092b27ddbb5a8be8cec1d8
LeeSF03 <shuenfeilee@gmail.com>
2025-02-07 22:20:21 +0800

    ci: remove unused variable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
-import { pages } from '@/constants/global'
 import { DropdownMenuNav } from '@/components/DropdownMenuNav'
 
 const bg =
