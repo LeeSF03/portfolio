93f33ca221241cb88f4db5dd76328aabda05a1f2
LeeSF03 <shuenfeilee@gmail.com>
2025-02-10 01:03:01 +0800

    refactor: slight change to CodeBlock component

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -7,7 +7,7 @@ import { LucideChevronsDown, LucideChevronsUp } from 'lucide-react'
 type CodeBlockProps = {
   code: string
 }
-export default function CodeBlock({}: CodeBlockProps) {
+export default function CodeBlock({ code }: CodeBlockProps) {
   //=====States=====
   const [open, setOpen] = useState(false)
 
@@ -18,36 +18,21 @@ export default function CodeBlock({}: CodeBlockProps) {
 
   return (
     <div className="relative mt-3 rounded-xl border-2 border-pink-300 bg-pink-400">
-      <div className={cn('h-32 overflow-hidden px-7', { 'h-auto': open })}>
+      <div
+        className={cn('h-32 overflow-hidden px-7', {
+          'h-auto': open,
+        })}
+      >
         <code>
-          <pre className="text-wrap text-left">{`
-FROM node:22-alpine AS deps
-WORKDIR /app
-COPY package.json yarn.lock ./
-RUN yarn install
-
-FROM node:22-alpine AS builder
-WORKDIR /app
-COPY --from=deps /app/node_modules ./node_modules
-COPY . ./
-RUN yarn build
-
-FROM node:22-alpine
-WORKDIR /app
-COPY --from=builder /app/.next ./.next
-COPY --from=builder /app/node_modules ./node_modules
-COPY --from=builder /app/package.json ./package.json
-COPY --from=builder /app/public ./public
-COPY --from=builder /app/next.config.ts ./next.config.ts
-EXPOSE 3000
-CMD ["yarn", "start"]
-
-`}</pre>
+          <pre className="text-wrap text-left">{code}</pre>
         </code>
       </div>
       <div
         onClick={onToggle}
-        className="group absolute bottom-0 flex h-8 w-full items-center justify-center rounded-b-xl bg-gradient-to-t from-pink-300 to-transparent hover:cursor-pointer"
+        className={cn(
+          'group absolute bottom-0 flex h-8 w-full items-center justify-center rounded-b-xl hover:cursor-pointer',
+          { 'bg-gradient-to-t from-pink-300 to-transparent': !open }
+        )}
       >
         {open ? (
           <LucideChevronsUp className="-motion-translate-y-in-[5px] group-hover:-motion-translate-y-out-[5px]" />
