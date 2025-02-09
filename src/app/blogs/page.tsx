5ce6dcf2f77bbd3f73614608d3232c8da204c225
LeeSF03 <shuenfeilee@gmail.com>
2025-02-10 00:42:56 +0800

    feat: use BlogCard in blogs page

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,25 +1,14 @@
 import { publicRoot } from '@/constants/global'
-import Image from 'next/image'
+import BlogCard from '@/components/BlogCard'
 
 export default function BlogPage() {
   return (
-    <div className="pt-3">
-      <div className="rounded-3xl border-4 border-gray-800 bg-green-300 p-3 shadow-[7px_7px_lightblue] hover:-motion-translate-x-out-[5px] hover:-motion-translate-y-out-[5px]">
-        <div className="flex h-64 flex-col overflow-hidden rounded-2xl border-4 border-gray-800">
-          <div className="h-40 w-full rounded-2xl">
-            <Image
-              src={`${publicRoot}/img/blogs/deployment.png`}
-              alt={'deployment cover'}
-              width={1920}
-              height={1080}
-            />
-          </div>
-          <div className="font-blog-title z-10 flex-1 bg-orange-300 p-3 text-xl font-semibold">
-            An upcoming blog about how I selfhost my own website using Azure,
-            Docker, Ansible, Nginx, GitHub and more...
-          </div>
-        </div>
-      </div>
+    <div className="flex flex-col gap-y-5 pt-3">
+      <BlogCard
+        blogId="deployment"
+        imageHref={`${publicRoot}/img/blogs/deployment.png`}
+        blogTitle="Deploying a NextJS website with Docker, Nginx, Ansible and GitHub Actions "
+      />
     </div>
   )
 }
