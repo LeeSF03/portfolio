525d3f8736243a6ed8439705f2d0aba367ff5bdd
LeeSF03 <shuenfeilee@gmail.com>
2025-02-11 13:50:44 +0800

    fix: blog card image glitch

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -22,7 +22,6 @@ export default function BlogCard({
               alt={'deployment cover'}
               width={1920}
               height={1080}
-              fill={true}
             />
           </div>
           <div className="z-10 flex-1 bg-orange-300 p-3 font-blog-title text-xl font-semibold">
